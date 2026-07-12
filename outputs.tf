741bd6541ebb84c48b4896163064e9606bf39c25
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "site_recovery_vmware_replication_policy_associations" {
-  description = "All site_recovery_vmware_replication_policy_association resources"
-  value       = azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations
-}
 output "site_recovery_vmware_replication_policy_associations_name" {
-  description = "List of name values across all site_recovery_vmware_replication_policy_associations"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations : v.name]
+  description = "Map of name values across all site_recovery_vmware_replication_policy_associations, keyed the same as var.site_recovery_vmware_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations : k => v.name }
 }
 output "site_recovery_vmware_replication_policy_associations_policy_id" {
-  description = "List of policy_id values across all site_recovery_vmware_replication_policy_associations"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations : v.policy_id]
+  description = "Map of policy_id values across all site_recovery_vmware_replication_policy_associations, keyed the same as var.site_recovery_vmware_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations : k => v.policy_id }
 }
 output "site_recovery_vmware_replication_policy_associations_recovery_vault_id" {
-  description = "List of recovery_vault_id values across all site_recovery_vmware_replication_policy_associations"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations : v.recovery_vault_id]
+  description = "Map of recovery_vault_id values across all site_recovery_vmware_replication_policy_associations, keyed the same as var.site_recovery_vmware_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy_association.site_recovery_vmware_replication_policy_associations : k => v.recovery_vault_id }
 }
 
